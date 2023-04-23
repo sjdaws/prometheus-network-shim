64f1ec57665f622e4fdc4c5180ab609f12f9b973
Scott Dawson <scott@uu.net.nz>
2023-04-23 11:02:50 +1000

    Support multi-container pods

--- pkg/cri/containers.go
+++ pkg/cri/containers.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
 	corev1 "k8s.io/api/core/v1"
 	runtimev1 "k8s.io/cri-api/pkg/apis/runtime/v1"
 )
@@ -19,15 +18,12 @@ func (c *Cri) getContainerSpec(pod *corev1.Pod, ps *runtimev1.PodSandboxStatusRe
 		return nil, err
 	}
 
-	// There should be exactly one stat from the filter
-	if len(stats) != 1 {
-		message := fmt.Sprintf("container stats returned should be 1, but got %d", len(stats))
-		if len(stats) == 0 {
-			message = fmt.Sprintf("%s - might be terminated/completed (phase: %s)", message, pod.Status.Phase)
-		}
-		return nil, errors.New(message)
+	// There should be at least one stat from the filter
+	if len(stats) < 1 {
+		return nil, errors.New(fmt.Sprintf("container stats returned should be 1, but got %d - might be terminated/completed (phase: %s)", len(stats), pod.Status.Phase))
 	}
 
+	// Always use stats for first container
 	cs, err := c.runtimeService.ContainerStatus(context.TODO(), stats[0].GetAttributes().GetId(), true)
 	if err != nil {
 		return nil, err
